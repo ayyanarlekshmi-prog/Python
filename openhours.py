03119a586fd807de623bd9972d6620cbf2a86e6a
Lekshmi <muthulekshmi94@gmail.com>
2026-05-27 12:25:16 -0700

    Control statements

---
+++ openhours.py
@@ -0,0 +1,24 @@
+time = 3
+if time >= 1 and time <=10:
+
+    print (" Store is open")
+else:
+    print("Store is closed")
+
+tanklevel = 5
+if tanklevel < 10:
+    print ("Fuel level is low")
+
+badge = True
+if badge:
+    print ("Access granted")
+else:
+    print("Access denied")
+
+age = 13
+if age >= 18:
+    print("Adult")
+elif 13 <= age <= 18:
+    print("Teen")
+
+
